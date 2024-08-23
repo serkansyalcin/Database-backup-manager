8759aeeebd452aebe0fc551c96e8387359fc7d10
serkansyalcin <serkan.syalcin@hotmail.com>
2024-08-23 11:19:54 +0300

    BackupServiceProvider Configuration.

--- src/Providers/BackupServiceProvider.php
+++ src/Providers/BackupServiceProvider.php
@@ -2,16 +2,21 @@
 namespace DatabaseBackupManager\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use DatabaseBackupManager\Console\Commands\BackupDatabase;
 
 class BackupServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        $this->commands([
+            BackupDatabase::class,
+        ]);
     }
 
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__.'/../Config/backup.php' => config_path('backup.php'),
+        ], 'config');
     }
 }
